9eefc2fa14e88ba20161e016183a5cfee2bdba4b
Poseidon <armaan_0517@yahoo.com>
2025-03-23 01:12:54 +0530

    Polished up the square text color inverse to make it work perfectly in all situations

--- src/App.js
+++ src/App.js
@@ -5,14 +5,23 @@ import { useState } from "react";
 
 function App() {
 	const [color, setColor] = useState("");
-	const square = document.querySelector(".square");
+	const garbageDiv = document.querySelector("#garbage");
 	const [squareTextColor, setSquareTextColor] = useState("whitesmoke");
 	const handleSquareTextColorChange = (inputColor) => {
 		setSquareTextColor(() => {
 			if (inputColor === "") return "whitesmoke";
+			const temp = document.createElement("div");
+			temp.style.backgroundColor = inputColor;
+			garbageDiv.innerHTML = "";
+			garbageDiv.appendChild(temp);
+			// console.log(temp);
 			const colorRGB = window
-				.getComputedStyle(square)
+				.getComputedStyle(temp, null)
 				.getPropertyValue("background-color");
+			if (colorRGB === "rgba(0, 0, 0, 0)") {
+				console.log("Not a color");
+				return squareTextColor;
+			}
 			const startingVal = colorRGB.includes("rgba") ? 5 : 4;
 			const colorBrightness = colorRGB
 				.slice(startingVal, -1)
